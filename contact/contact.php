deb9d7b0771f2a387c3781e9f8b6b5abe0d91001
Rudolf <kulcsarrudolf@gmail.com>
2017-10-23 22:36:39 +0300

    Version 1.7

--- contact/contact.php
+++ contact/contact.php
@@ -8,9 +8,13 @@ if( isset($_POST['name']) && isset($_POST['email']) && isset($_POST['tel']) && i
 	$from = $email;
 	$subject = 'New message - '.$name;
 	$message = '<b>Name:</b> '.$name.' <br><b>Email:</b> '.$email.'<br><b>Tel:</b> '.$tel.'<br><br> <p>'.$message.'</p>';
-	$headers = "From: $from\n";
-	$headers .= "MIME-Version: 1.0\n";
-	$headers .= "Content-type: text/html; charset=iso-8859-1\n";
+	
+	$headers = "MIME-Version: 1.0" . "\r\n";
+	$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+	$headers .= 'From: "MEGAMUTARI" <'.$to.'>' . "\r\n";
+	$headers .= "Reply-To: ".$to."\r\n";
+	$headers .= "Return-Path: ".$to."\r\n";
+
 	if( mail($to, $subject, $message, $headers) ){
 		echo "Message sent!";
 	} else {
